76c543408009c697a805dded983ac749d2b74dcf
AShaww <71328022+AShaww@users.noreply.github.com>
2022-06-10 00:18:27 +0100

    Added Note class for controller Entity scaffolding

---
+++ TakeNote/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TakeNote.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
