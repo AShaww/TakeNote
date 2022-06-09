d1db507139fe2ae380ff7c2b5bdd376d479d0d87
AShaww <71328022+AShaww@users.noreply.github.com>
2022-06-10 00:21:09 +0100

    Added NotesController using Entity scaffolding from Notes class

---
+++ TakeNote/Data/TakeNoteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TakeNote.Models;
+
+namespace TakeNote.Data
+{
+    public class TakeNoteContext : DbContext
+    {
+        public TakeNoteContext (DbContextOptions<TakeNoteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TakeNote.Models.Note>? Note { get; set; }
+    }
+}
--- TakeNote/Program.cs
+++ TakeNote/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TakeNote.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TakeNoteContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TakeNoteContext") ?? throw new InvalidOperationException("Connection string 'TakeNoteContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
