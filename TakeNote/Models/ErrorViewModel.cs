ef0620c2bd27be6471f67fd683a01864b0e76c79
AShaww <71328022+AShaww@users.noreply.github.com>
2022-06-10 00:16:26 +0100

    Add project files.

---
+++ TakeNote/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TakeNote.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
